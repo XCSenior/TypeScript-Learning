1bdd85ba01912e49c899b7399559f1c5512301e5
XCSenior <2969271422@qq.com>
2022-09-06 16:39:02 +0800

    Update 1_类的简介.ts

--- MyTypeScriptExercise/3、面向对象/1.类(class)/1_类的简介.ts
+++ MyTypeScriptExercise/3、面向对象/1.类(class)/1_类的简介.ts
@@ -6,25 +6,26 @@
 */
 class Person {
     /*
-    *   1、直接定义的属性是实例属性，需要通过对象的实例去访问：
+    *   1、直接定义的属性是实例属性，需要通过new对象的实例去访问：
     *       const per = new Person()
     *       per.name
     *
     *   2、使用static开头的属性是静态属性（类属性），可以直接通过类去访问
+    *       不需要创建对象即可访问的属性
     *       Person.age
     *
-    *   3、readonly开头：属性表示一个只读的属性无法修改
+    *   3、readonly开头：属性表示一个只读的属性无法修改，需要放在static后面
     */
 
     /* **********************定义属性************************** */
     //定义实例属性
     // readonly name = '孙悟空';
-    name = '孙悟空';
-    age = 18
+    name : string = '孙悟空';
+    age : number = 18;
 
     //定义静态属性(类属性)
     // static readonly staticAge = 18
-    static staticAge = 'static18'
+    static staticAge = 'static18';
 
     /* **********************定义方法************************* */
     //定义实例方法
@@ -39,5 +40,5 @@ class Person {
 
 const per = new Person();
 
-per.sayHello()
-Person.staticSayHello()
+per.sayHello();
+Person.staticSayHello();
