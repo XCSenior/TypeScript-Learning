2493c519f455c9a8bfa5c50566fc3711283f5db6
XCSenior <2969271422@qq.com>
2022-09-07 19:15:31 +0800

    学习构造函数简写形式

--- MyTypeScriptExercise/3、面向对象/1.类(class)/2_构造函数.ts
+++ MyTypeScriptExercise/3、面向对象/1.类(class)/2_构造函数.ts
@@ -4,7 +4,7 @@ class Dog {
 
     // constructor 被称为构造函数
     // 构造函数会在对象创建时调用，即new出一个对象的时候
-    constructor (name:string, age:number){
+    constructor(name: string, age: number) {
         // 在实例方法中,this就表示当前实例
         // 构造函数中当前对象就是当前新建的那个对象
         //      可以通过this向新建的对象中添加属性
@@ -12,7 +12,7 @@ class Dog {
         this.age = age;
     }
 
-    bark (){
+    bark() {
         alert("汪汪汪");
         // 在实例方法中,通过this来表示当前调用方法的对象
         console.log(this);
@@ -21,3 +21,15 @@ class Dog {
 
 const dog = new Dog("小黑", 4);
 
+/* 构造函数和属性声明可以简写 */
+class Dog2 {
+    /** 相当于完成了
+     *  name: string;
+        age: number;
+    *   constructor(name: string, age: number) {
+            this.name = name;
+            this.age = age;
+        }
+    */
+    constructor(public name: string, public age: number) {}
+}
