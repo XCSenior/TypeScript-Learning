13a311a3b675bfe292b6b9c39f99a9023f264482
XCSenior <2969271422@qq.com>
2022-09-07 16:38:49 +0800

    Create 1_封装（属性封装）.ts

---
+++ MyTypeScriptExercise/3、面向对象/2.面向对象特点/1_封装（属性封装）.ts
@@ -0,0 +1,13 @@
+(function () {
+    // 首先定义一个表示人的类
+    class Person {
+        name: string;
+        age: number;
+        constructor(name: string, age: number) {
+            this.name = name;
+            this.age = age;
+        }
+    }
+
+
+})();
