1310f7b6e57308a71dcd98a3188304e16f02afa0
XCSenior <2969271422@qq.com>
2022-09-27 21:02:01 +0800

    Update GameControl.ts

--- MyTypeScriptExercise/4、贪吃蛇小demo/src/modules/GameControl.ts
+++ MyTypeScriptExercise/4、贪吃蛇小demo/src/modules/GameControl.ts
@@ -10,6 +10,8 @@ export default class GameControl {
     scorePanel: ScorePanel;
     /* 用一个属性来存储蛇的移动方向(即按键的方向) */
     direction: string = '';
+    /* 用一个属性记录游戏是否结束 */
+    isLive: boolean = true;
     constructor() {
         this.snake = new Snake();
         this.food = new Food();
@@ -27,5 +29,40 @@ export default class GameControl {
     keydownHandler(event: KeyboardEvent) {
         /* 赋值之前需要检查event.key是否合法(是否按了正确的按键) */
         this.direction = event.key;
+    }
+
+    /* 创建snake控制移动的方法 */
+    run() {
+        /** 根据方向this.direction, 改变位置移动
+         *  向上 top减小   向下 top增加,  向左 left减小   向右 left增加
+         */
+        // 获取snake当前坐标
+        let X = this.snake.X;
+        let Y = this.snake.Y;
+        switch (this.direction) {
+            case 'ArrowUp':
+            case 'up':
+                Y -= 10;
+                break;
+            case 'ArrowDown':
+            case 'down':
+                Y += 10;
+                break;
+            case 'ArrowLeft':
+            case 'Left':
+                X -= 10;
+                break;
+            case 'ArrowRight':
+            case 'right':
+                X += 10;
+                break;
+        }
+
+        // 修改蛇的X和Y值
+        this.snake.X = X;
+        this.snake.Y = Y;
+
+        /* 随着等级越来越高, snake的速度越来越高 */
+        this.isLive && setTimeout(this.run.bind(this), 300 - (this.scorePanel.level - 1) * 30);
     }
 }
